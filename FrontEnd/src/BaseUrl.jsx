5c17520949086b9e9aa69305b8f93825b180c55e
Muhammad Farhan <mfarhanabbasi99@gmail.com>
2024-09-17 02:45:16 +0500

    hello

--- FrontEnd/src/BaseUrl.jsx
+++ FrontEnd/src/BaseUrl.jsx
@@ -3,4 +3,4 @@ import React from 'react'
 
 // export default "http://localhost:2000"
 // export default "https://protfolio-urlh.onrender.com"
-export default "https://protfolio-coral-nine.vercel.app"
+export default "https://protfolio-zeta-sand.vercel.app"
